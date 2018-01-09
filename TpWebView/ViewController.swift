02baa7b9dbb5a29bb9e004814fcf4e208419e540
chajia <chajia.bilal23@gmail.com>
2018-01-08 20:24:36 -0800

    Initial Commit

---
+++ TpWebView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TpWebView
+//
+//  Created by Bilal CHAJIA on 1/8/18.
+//  Copyright © 2018 Bilal CHAJIA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
